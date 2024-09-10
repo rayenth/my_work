af873d415b2e3b43485db1ddee10efc1105e0648
rayenth <153260307+rayenth@users.noreply.github.com>
2024-09-10 21:39:12 +0100

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,37 @@
+﻿from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+
+
+#list of functions :
+#-clean_by() :
+#
+#       input :text,text
+#       output :a list that is cleaned and is more readable
+#
+#-create_driver() : 
+#
+#       input : url, path 
+#       output : driver
+#  
+
+
+
+#this will split a text with a specific delimiter
+def clean_by(t,x):
+    l=t.split(x)
+    return l
+
+
+
+
+#this will create a chrome driver 
+def create_driver(url,path):
+    options=Options()
+    options.add_argument("--headless")
+    options.add_argument("--log-level=3")
+    service_0=Service(executable_path=path)
+    driver=webdriver.Chrome(service=service_0,options=options)
+    driver.get(url)
+    return driver
+
---
+++ to_exel.py
@@ -0,0 +1,45 @@
+﻿from openpyxl import Workbook
+from openpyxl.styles import Alignment,Font
+from openpyxl.utils import get_column_letter
+
+
+
+#this fuction will create an excel file and fill it with the data from the dictionnary
+def make_excel(dic):
+    wb=Workbook()
+    ws=wb.create_sheet("GPU_INFO")
+    ws.append(list(dic.keys()))
+    for i in range (0,len(list(dic["Memory"][1]))):
+        l=[]
+        for j in list(dic.keys()): 
+            l.append(dic[j][1][i])
+            
+        ws.append(l)
+
+    apply_text_wrapping(ws)
+    adjust_column_widths(ws, padding=5)
+
+#making a font for the header
+    for column_num in range (1,len(dic.keys())+1):
+        col_letter=get_column_letter(column_num)
+        ws[f"{col_letter}1"].font=Font(bold=True,color="ffa000")
+
+
+    wb.save("GPU_Data.xlsx")
+
+
+#this will show the entire text in the cell
+def apply_text_wrapping(ws):
+    for row in ws.iter_rows():
+        for cell in row:
+            cell.alignment = Alignment(wrap_text=True)
+
+
+#and this will desplay the text in a single ligne instead of desplaying it in a wrapped manner 
+def adjust_column_widths(ws, padding=2):
+    for col in ws.columns:
+        max_length = max(len(str(cell.value)) for cell in col)
+        adjusted_width = max_length + padding
+        col_letter = get_column_letter(col[0].column)
+        ws.column_dimensions[col_letter].width = adjusted_width
+    
